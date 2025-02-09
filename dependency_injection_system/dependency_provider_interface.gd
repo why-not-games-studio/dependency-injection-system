61abf3d134d4b170bfc7eabd429c25bd340b158c
Vince Russell Recentes <76193542+comfygrapes@users.noreply.github.com>
2025-02-09 12:43:49 +0800

    Integrated re-implementation of di system that relies on node_added signal

--- dependency_injection_system/dependency_provider_interface.gd
+++ dependency_injection_system/dependency_provider_interface.gd
@@ -1,14 +1,13 @@
-## Serves as an abstraction between any DependencyProvider and its dependents 
-## and must be autoloaded as global if Dependency Injection functionality is desired.
-## Global - IDependencyProvider
+## Serves as an Abstraction between any [DependencyProvider] and its dependents 
+## and MUST be a Global Autoload if Dependency Injection functionality is desired.
+## IDependencyProvider
 extends Node
 
 
 signal dependency_registration_requested(dependency: Node)
 signal dependency_deregistration_requested(dependency_name: StringName)
 
-signal dependency_provision_requested(dependency_names: Array[StringName])
-signal dependency_provision_completed(dependencies: Dictionary, result: Error)
+signal dependency_injection_requested
 
 
 func register_dependency(dependency: Node) -> void:
@@ -18,11 +17,5 @@ func deregister_dependency(dependency_name: StringName) -> void:
 	dependency_deregistration_requested.emit(dependency_name)
 
 
-func request_dependency_provision(dependency_names: Array[StringName]) -> void:
-	dependency_provision_requested.emit(dependency_names)
-
-func succeed_dependency_provision(dependencies: Dictionary) -> void:
-	dependency_provision_completed.emit(dependencies, OK)
-
-func fail_dependency_provision(error: Error) -> void:
-	dependency_provision_completed.emit({}, error)
+func request_dependency_injection() -> void:
+	dependency_injection_requested.emit()
--- dependency_injection_system/test_receiver.gd
+++ dependency_injection_system/test_receiver.gd
@@ -2,4 +2,4 @@ extends Node
 
 
 func _inject(pause_manager: PauseManagerBase, game_manager: GameManagerBase) -> void:
-	ILogger.log_debug(str(pause_manager, " ", game_manager), str(_inject))
+	ILogger.log_debug(str(name, " ", pause_manager, " ", game_manager), str(_inject))
