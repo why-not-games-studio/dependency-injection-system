67346ceef6fa6d82c5f94ec2d6b6ea3d6c4e0856
Vince Russell Recentes <brusrecenthusky@gmail.com>
2025-05-03 19:06:51 +0800

    Made plugin remove nodes by name on disable

--- addons/simple_dependency_injection/simple_dependency_injection.gd
+++ addons/simple_dependency_injection/simple_dependency_injection.gd
@@ -2,30 +2,29 @@
 extends EditorPlugin
 
 
-var _dependency_registrar: DependencyRegistrarBase
-var _dependency_provider: DependencyProvider
-
-
 func _enable_plugin() -> void:
 	var scene_root = get_editor_interface().get_edited_scene_root()
 	if scene_root:
-		_dependency_registrar = DependencyRegistrar.new()
-		_dependency_provider = DependencyProvider.new()
-		_dependency_provider.set_dependency_registrar(_dependency_registrar)
+		var dependency_registrar = DependencyRegistrar.new()
+		var dependency_provider = DependencyProvider.new()
+		dependency_provider.set_dependency_registrar(dependency_registrar)
 		
-		_dependency_registrar.name = "DependencyRegistrar"
-		_dependency_provider.name = "DependencyProvider"
+		dependency_registrar.name = "DependencyRegistrar"
+		dependency_provider.name = "DependencyProvider"
 		
-		scene_root.add_child(_dependency_registrar)
-		scene_root.add_child(_dependency_provider)
+		scene_root.add_child(dependency_registrar)
+		scene_root.add_child(dependency_provider)
 		
-		_dependency_registrar.owner = scene_root
-		_dependency_provider.owner = scene_root
+		dependency_registrar.owner = scene_root
+		dependency_provider.owner = scene_root
 
 func _disable_plugin() -> void:
-	if _dependency_registrar and _dependency_registrar.is_inside_tree() \
-		and _dependency_provider and _dependency_provider.is_inside_tree():
-			_dependency_registrar.queue_free()
-			_dependency_provider.queue_free()
-			_dependency_registrar = null
-			_dependency_provider = null
+	var scene_root = get_editor_interface().get_edited_scene_root()
+	if scene_root:
+		var registrar_node: Node = scene_root.find_child("DependencyRegistrar")
+		if registrar_node and registrar_node is DependencyRegistrar:
+			registrar_node.queue_free()
+
+		var provider_node = scene_root.find_child("DependencyProvider")
+		if provider_node and provider_node is DependencyProvider:
+			provider_node.queue_free()
